76e5418b2e36ad317ed6eb421f352c884e12abd6
qianj <qj95julia@163.com>
2021-06-06 22:25:16 +0800

    2021.6.6

--- com.imooc/mybatis/src/test/java/com/imooc/mybatis/MyBatisTestor.java
+++ com.imooc/mybatis/src/test/java/com/imooc/mybatis/MyBatisTestor.java
@@ -1,7 +1,10 @@
 package com.imooc.mybatis;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.imooc.mybatis.dto.GoodsDTO;
 import com.imooc.mybatis.entity.Goods;
+import com.imooc.mybatis.entity.GoodsDetail;
 import com.imooc.mybatis.utils.MyBatisUtils;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -12,17 +15,15 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.Reader;
 import java.sql.Connection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MyBatisTestor {
     @Test
-    public void testMyBatisUtils(){
+    public void testMyBatisUtils() {
         SqlSession sqlSession = null;
         try {
             sqlSession = MyBatisUtils.openSession();
-            Connection connection =  sqlSession.getConnection();
+            Connection connection = sqlSession.getConnection();
             System.out.println(connection);
         } catch (Exception e) {
             e.printStackTrace();
@@ -31,6 +32,7 @@ public class MyBatisTestor {
             MyBatisUtils.closeSession(sqlSession);
         }
     }
+
     @Test
     public void testSqlSessionFactory() throws IOException {
         // 利用Reader加载classpath下的mybatis-config.xml核心配置文件
@@ -39,40 +41,41 @@ public class MyBatisTestor {
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
         System.out.println("SqlSessionFactory加载成功");
         SqlSession sqlSession = null;
-        try{
+        try {
             //创建SqlSession对象，SqlSession是JDBC的扩展类，用于与数据库交互
             sqlSession = sqlSessionFactory.openSession();
             //非必须，演示创建数据库连接过程，mybatis自动生成 不出现java.sql包的导入
             Connection connection = sqlSession.getConnection();
             System.out.println(connection);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
-            if(sqlSession!=null){
+        } finally {
+            if (sqlSession != null) {
                 // 如果mybatis-config文件中type="POOLED“,代表使用连接池,close是将连接回收至连接池
                 // 如果type="UNPOOLED",代表直连,close则会调用Connection.close()方法关闭连接
                 sqlSession.close();
             }
         }
     }
+
     @Test
     public void testSelectAll() throws Exception {
         SqlSession session = null;
         try {
             session = MyBatisUtils.openSession();
             List<Goods> list = session.selectList("goods.selectAll");
-            for(Goods g:list){
+            for (Goods g : list) {
                 System.out.println(g.getTitle());
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             throw e;
-        }finally {
+        } finally {
             MyBatisUtils.closeSession(session);
         }
     }
 
     @Test
-    public void testSelectById(){
+    public void testSelectById() {
         SqlSession session = null;
         try {
             session = MyBatisUtils.openSession();
@@ -86,7 +89,7 @@ public class MyBatisTestor {
     }
 
     @Test
-    public void testSelectByPriceRange(){
+    public void testSelectByPriceRange() {
         SqlSession session = null;
         try {
             session = MyBatisUtils.openSession();
@@ -95,8 +98,8 @@ public class MyBatisTestor {
             param.put("max", 500);
             param.put("limit", 10);
             List<Goods> goodsList = session.selectList("selectByPriceRange", param);
-            for(Goods g:goodsList){
-                System.out.println(g.getTitle()+":"+g.getCurrentPrice());
+            for (Goods g : goodsList) {
+                System.out.println(g.getTitle() + ":" + g.getCurrentPrice());
             }
         } catch (Exception e) {
             throw e;
@@ -106,12 +109,12 @@ public class MyBatisTestor {
     }
 
     @Test
-    public void testSelectGoodsMap(){
+    public void testSelectGoodsMap() {
         SqlSession session = null;
         try {
             session = MyBatisUtils.openSession();
-            List<Map> list =  session.selectList("goods.selectGoodsMap");
-            for(Map map:list){
+            List<Map> list = session.selectList("goods.selectGoodsMap");
+            for (Map map : list) {
                 System.out.println(map);
             }
         } catch (Exception e) {
@@ -120,13 +123,14 @@ public class MyBatisTestor {
             MyBatisUtils.closeSession(session);
         }
     }
+
     @Test
-    public void testSelectGoodsDTO(){
+    public void testSelectGoodsDTO() {
         SqlSession session = null;
         try {
             session = MyBatisUtils.openSession();
             List<GoodsDTO> list = session.selectList("goods.selectGoodsDTO");
-            for(GoodsDTO goods:list){
+            for (GoodsDTO goods : list) {
                 System.out.println(goods.getGoods().getTitle());
             }
         } catch (Exception e) {
@@ -135,8 +139,9 @@ public class MyBatisTestor {
             MyBatisUtils.closeSession(session);
         }
     }
+
     @Test
-    public void testInsert() throws Exception{
+    public void testInsert() throws Exception {
         SqlSession session = null;
         try {
             session = MyBatisUtils.openSession();
@@ -153,41 +158,7 @@ public class MyBatisTestor {
             session.commit();//提交事务数据
             System.out.println(goods.getGoodsId());
         } catch (Exception e) {
-            if(session!=null){
-                session.rollback();//回滚事务
-            }
-            throw e;
-        } finally {
-            MyBatisUtils.closeSession(session);
-        }
-    }
-    @Test
-    public void testUpdate() throws Exception{
-        SqlSession session = null;
-        try {
-            session = MyBatisUtils.openSession();
-            Goods goods = session.selectOne("goods.selectById", 739);
-            goods.setTitle("更新的测试商品");
-            int num = session.update("goods.update", goods);
-            session.commit();
-        } catch (Exception e) {
-            if(session!=null){
-                session.rollback();//回滚事务
-            }
-            throw e;
-        } finally {
-            MyBatisUtils.closeSession(session);
-        }
-    }
-    @Test
-    public void testDelete() throws Exception{
-        SqlSession session = null;
-        try {
-            session = MyBatisUtils.openSession();
-            int num = session.delete("goods.delete", 739);
-            session.commit();
-        } catch (Exception e) {
-            if(session!=null){
+            if (session != null) {
                 session.rollback();//回滚事务
             }
             throw e;
@@ -197,23 +168,122 @@ public class MyBatisTestor {
     }
 
     @Test
-    public void testSelectByTitle() throws Exception{
+    public void testUpdate() throws Exception {
+        SqlSession session = null;
+        try {
+            session = MyBatisUtils.openSession();
+            Goods goods = session.selectOne("goods.selectById", 739);
+            goods.setTitle("更新的测试商品");
+            int num = session.update("goods.update", goods);
+            session.commit();
+        } catch (Exception e) {
+            if (session != null) {
+                session.rollback();//回滚事务
+            }
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(session);
+        }
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        SqlSession session = null;
+        try {
+            session = MyBatisUtils.openSession();
+            int num = session.delete("goods.delete", 739);
+            session.commit();
+        } catch (Exception e) {
+            if (session != null) {
+                session.rollback();//回滚事务
+            }
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(session);
+        }
+    }
+
+    @Test
+    public void testSelectByTitle() throws Exception {
         SqlSession session = null;
         try {
             session = MyBatisUtils.openSession();
             Map param = new HashMap();
             param.put("title", "''or 1=1 or title='爱恩幼 孕妇护肤品润养颜睡眠面膜 100g'");
-            List<Goods> list = session.selectList("goods.selectByTitle",param);
-            for(Goods g:list){
-                System.out.println(g.getTitle()+":"+g.getCurrentPrice());
+            List<Goods> list = session.selectList("goods.selectByTitle", param);
+            for (Goods g : list) {
+                System.out.println(g.getTitle() + ":" + g.getCurrentPrice());
             }
             session.commit();
         } catch (Exception e) {
-            if(session!=null){
+            if (session != null) {
                 session.rollback();//回滚事务
             }
             throw e;
         } finally {
+            MyBatisUtils.closeSession(session);
+        }
+    }
+
+    /**
+     * PageHelper分页查询
+     */
+    @Test
+    public void testPageHelper() throws Exception{
+        SqlSession session = null;
+        try {
+            session = MyBatisUtils.openSession();
+//            startPage方法会自动将下一次查询进行分页
+            PageHelper.startPage(2, 10);
+            Page<Goods> page = (Page) session.selectList("goods.selectPage");
+            System.out.println("总页数：" + page.getPages());
+            System.out.println("总记录数：" + page.getTotal());
+            System.out.println("开始行号：" + page.getStartRow());
+            System.out.println("结束行号：" + page.getEndRow());
+            System.out.println("当前页码：" + page.getPageNum());
+            List<Goods> data = page.getResult(); //当前页数据
+            for (Goods g: data){
+                System.out.println(g.getTitle());
+            }
+            System.out.println("");
+        }catch (Exception e){
+            throw e;
+        }finally {
+            MyBatisUtils.closeSession(session);
+        }
+    }
+
+    /**
+     * 批量插入测试
+     */
+    public void testBatchInsert() throws Exception{
+        SqlSession session = null;
+        try {
+            long st = new Date().getTime();
+            session = MyBatisUtils.openSession();
+            List list = new ArrayList();
+            for (int i=0; i<10; i++){
+                Goods goods = new Goods();
+                goods.setTitle("批量插入商品");
+                goods.setSubTitle("批量插入子标题");
+                goods.setOriginalCost(200f);
+                goods.setCurrentPrice(100f);
+                goods.setDiscount(0.5f);
+                goods.setIsFreeDelivery(1);
+                goods.setCategoryId(43);
+                // insert()方法返回值代表本次成功插入的记录总数
+                list.add(goods);
+            }
+            session.insert("goods.batchInsert", list);
+            session.commit(); // 提交事务数据
+            long et = new Date().getTime();
+            System.out.println("执行时间：" + (et - st) + "毫秒");
+        }catch (Exception e){
+            if(session != null){
+                session.rollback();
+            }
+            throw e;
+        }finally {
             MyBatisUtils.closeSession(session);
         }
     }
